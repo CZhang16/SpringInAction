0b6bc303bc1436b10395875052ab7a4c5bd53d13
zhang chong <czhang1@leqee.com>
2018-04-13 15:58:30 +0800

    init

---
+++ src/main/java/spring/soundsystem/CompactDisc.java
@@ -0,0 +1,5 @@
+package spring.soundsystem;
+
+public interface CompactDisc {
+    void play();
+}
---
+++ src/main/java/spring/soundsystem/SgtPeppers.java
@@ -0,0 +1,14 @@
+package spring.soundsystem;
+
+import org.springframework.stereotype.Component;
+
+import javax.inject.Named;
+
+//@Named("lonelyHeartsClub") 与下条功能大致相同
+@Component("lonelyHeartsClub")
+public class SgtPeppers implements CompactDisc {
+
+    public void play() {
+        System.out.println("Sgtpeppers");
+    }
+}
